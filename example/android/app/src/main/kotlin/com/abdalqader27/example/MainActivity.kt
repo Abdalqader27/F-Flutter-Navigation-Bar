6d5d25ac53f122e3b04fff7dcef0737e271334fd
abdalqader <abdalqader27.najjar@gmail.com>
2021-05-02 08:45:49 +0000

    push library

---
+++ example/android/app/src/main/kotlin/com/abdalqader27/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abdalqader27.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:f_navigation_bar/f_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ff_navigation_bar Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ff_navigation_bar Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Demonstration',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: FNavigationBar(
+        theme: FNavigationBarTheme(
+          barBackgroundColor: Colors.white,
+          selectedItemBorderColor: Colors.transparent,
+          selectedItemBackgroundColor: Colors.green,
+          selectedItemIconColor: Colors.white,
+          selectedItemLabelColor: Colors.black,
+          showSelectedItemShadow: false,
+          barHeight: 70,
+        ),
+        selectedIndex: selectedIndex,
+        onSelectTab: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          FNavigationBarItem(
+            iconData: Icons.calendar_today,
+            label: 'Bar Theme',
+          ),
+          FNavigationBarItem(
+            iconData: Icons.people,
+            label: 'Orange',
+            selectedBackgroundColor: Colors.orange,
+          ),
+          FNavigationBarItem(
+            iconData: Icons.attach_money,
+            label: 'Purple',
+            selectedBackgroundColor: Colors.purple,
+          ),
+          FNavigationBarItem(
+            iconData: Icons.note,
+            label: 'Blue',
+            selectedBackgroundColor: Colors.blue,
+          ),
+          FNavigationBarItem(
+            iconData: Icons.settings,
+            label: 'Red Item',
+            selectedBackgroundColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
